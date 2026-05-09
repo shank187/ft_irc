32ea99b3f962725a75433ba7497b891411d25c13
shank187 <153318667+shank187@users.noreply.github.com>
2026-05-09 11:55:51 +0000

    working on the buffering the packets

--- testing.cpp
+++ testing.cpp
@@ -8,6 +8,7 @@
 #include <fcntl.h>
 #include <vector>
 #include <poll.h>
+#include <map>
 
 
 int main(void)
@@ -40,20 +41,90 @@ int main(void)
 
     struct pollfd server_pollfd;// the specific struct the os require to monitor socket
     server_pollfd.fd = server_fd;// passing our main server door to it
-    server_pollfd.events = POLLIN; // 
-    server_pollfd.revents = 0;
+    server_pollfd.events = POLLIN; // "poll inbound",only wake up the server when data comes IN
+    server_pollfd.revents = 0;//"returned events" when ll put our server asleep, it monitors the sockets, 
+    //when it wakes up the operating system automatically changes the value of revent to tell exactly what
+    //happened, by setting it to 0 when ensure we dont accidentaly read old data from previous loop.
     fds.push_back(server_pollfd);
-
+    std::map<int, std::string> client_buffer;
     while(true)
     {
+        // 1. Wait for someone to knock or send a message
         int poll_count = poll(&fds[0], fds.size(), -1);
         if(poll_count == -1)
         {
             std::cerr << "poll error!" << std::endl;
             break;
         }
-        std::cout << "someone woke the server up" << std::endl;
-    }
+        
+        // 2. Someone woke us up! Let's find out WHO it was.
+        // We loop through all the buzzers we currently have.
+        for (size_t i = 0; i < fds.size(); i++) 
+        {
+            // The OS sets POLLIN in 'revents' if this specific socket has data ready
+            if (fds[i].revents & POLLIN) 
+            {
+                // CASE A: The buzzer that went off is our main server door!
+                if (fds[i].fd == server_fd) 
+                {
+                    struct sockaddr_in client_address;
+                    socklen_t client_len = sizeof(client_address);
+                    
+                    // OPEN THE DOOR! (This clears the POLLIN state so it stops infinitely looping)
+                    int client_fd = accept(server_fd, (struct sockaddr *)&client_address, &client_len);
+                    
+                    if (client_fd != -1) 
+                    {
+                        std::cout << "Success! A new client walked in! FD: " << client_fd << std::endl;
+                        
+                        // Rule 1: Make the new client non-blocking too!
+                        fcntl(client_fd, F_SETFL, O_NONBLOCK);
+                        
+                        // Rule 2: Give the new client their own buzzer and add them to our list
+                        struct pollfd client_pollfd;
+                        client_pollfd.fd = client_fd;
+                        client_pollfd.events = POLLIN; // Tell poll() to watch for their messages
+                        client_pollfd.revents = 0;
+                        fds.push_back(client_pollfd);
+                    }
+                }
+                // CASE B: A Client Table Buzzed (They sent a message)
+                else {
+                    char buffer[1024];
+                    std::memset(buffer, 0, sizeof(buffer)); // Always clean the buffer first!
+
+                    // 1. Take the order (read from the socket)
+                    int bytes_received = recv(fds[i].fd, buffer, sizeof(buffer) - 1, 0);
+
+                    // 2. Did they leave the restaurant? (Ctrl+C or disconnect)
+                    if (bytes_received <= 0) {
+                        std::cout << "Client " << fds[i].fd << " disconnected." << std::endl;
+                        
+                        close(fds[i].fd);               // Close their specific socket
+                        fds.erase(fds.begin() + i);     // Remove their buzzer from the list
+                        i--;                            // Adjust the loop index since we shrank the list!
+                    } 
+                    // 3. They actually said something!
+                    else {
+                        client_buffer[fds[i].fd] += buffer;
+                        size_t pos;
+                        while((pos = client_buffer[fds[i].fd].find('\n')) != std::string::npos)
+                        {
+                            
+                        }
+                        if(client_buffer[fds[i].fd].find('\n') != std::string::npos)
+                        {
+                            std::cout << "Client " << fds[i].fd << " says: " << client_buffer[fds[i].fd];
+                            
+                            std::string reply = "Server heard you!\n";
+                            send(fds[i].fd, reply.c_str(), reply.length(), 0);
+                            client_buffer[fds[i].fd].clear();
+                        }
+                    }
+                }
+            }
+        }
+    }    
     close(server_fd);
     return 0;
 }
