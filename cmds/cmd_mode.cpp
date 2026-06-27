5ecaaedde69086da60db1df63093a07306afc78f
Abdelhamid El Haddadi <abel-had@e2r4p9.1337.ma>
2026-06-27 15:55:29 +0100

    brodcasted mode

--- cmds/cmd_mode.cpp
+++ cmds/cmd_mode.cpp
@@ -1,4 +1,5 @@
 #include "../Core.hpp"
+#include <cstdlib>
 
 void Core::cmd_mode(Client* client, mssg& msg)
 {
@@ -30,7 +31,7 @@ void Core::cmd_mode(Client* client, mssg& msg)
 
     if (it != channels.end())
     {
-        if (msg.args.size() == 1)//read mode in this chanel
+        if (msg.args.size() == 1) // read mode in this chanel
         {
             std::string current_modes = it->second->get_modes();
             client->set_write_buffer(":localhost 324 " + client->get_nickname() + " " + target_channel + " " + current_modes + "\r\n");
@@ -41,8 +42,8 @@ void Core::cmd_mode(Client* client, mssg& msg)
             client->set_write_buffer(":localhost 368 " + client->get_nickname() + " " + target_channel + " :End of channel ban list\r\n");
             return;
         }
-        //set mode!!!!!!!!!!1
         
+        // set mode checks
         if (!it->second->is_operator(client))
         {
             client->set_write_buffer(":localhost 482 " + client->get_nickname() + " " + target_channel + " :You're not channel operator\r\n");
@@ -56,6 +57,10 @@ void Core::cmd_mode(Client* client, mssg& msg)
         
         std::string applied_modes = "";
         std::string applied_args = "";
+        
+ 
+        char current_sign = '+';
+        char last_appended_sign = '\0';
 
         for (size_t i = 0; i < mode_str.length(); i++)
         {
@@ -64,114 +69,132 @@ void Core::cmd_mode(Client* client, mssg& msg)
             if (c == '+')
             {
                 add = true;
-                applied_modes += "+";
+                current_sign = '+';
             }
             else if (c == '-')
             {
                 add = false;
-                applied_modes += "-";
+                current_sign = '-';
             }
-            else if (c == 'i')
+            else
             {
-                it->second->set_mode_i(add);
-                applied_modes += "i";
-            }
-            else if (c == 't')
-            {
-                it->second->set_mode_t(add);
-                applied_modes += "t";
-            }
-           else if (c == 'k')
-            {
-                if (add && arg_idx < msg.args.size())
-                {
-                    if (param_count >= 3) continue;
-                    param_count++;
+                bool mode_applied = false;
 
-                    if (!it->second->get_password().empty()) {
-                        client->set_write_buffer(":localhost 467 " + client->get_nickname() + " " + target_channel + " :Channel key already set\r\n");
-                    } else {
-                        it->second->set_password(msg.args[arg_idx]);
-                        applied_modes += "k";
-                        applied_args += " " + msg.args[arg_idx];
-                    }
-                    arg_idx++;
-                }
-                else if (!add)
+                if (c == 'i')
                 {
-                    if (arg_idx < msg.args.size())
+                    it->second->set_mode_i(add);
+                    mode_applied = true;
+                }
+                else if (c == 't')
+                {
+                    it->second->set_mode_t(add);
+                    mode_applied = true;
+                }
+                else if (c == 'k')
+                {
+                    if (add && arg_idx < msg.args.size())
                     {
-                        if (msg.args[arg_idx] == it->second->get_password())
-                        {
-                            it->second->set_password("");
-                            applied_modes += "k";
+                        if (param_count >= 3) continue;
+                        param_count++;
+
+                        if (!it->second->get_password().empty()) {
+                            client->set_write_buffer(":localhost 467 " + client->get_nickname() + " " + target_channel + " :Channel key already set\r\n");
+                        } else {
+                            it->second->set_password(msg.args[arg_idx]);
                             applied_args += " " + msg.args[arg_idx];
+                            mode_applied = true;
                         }
                         arg_idx++;
                     }
-                }
-            }
-            else if (c == 'l')
-            {
-                if (add && arg_idx < msg.args.size())
-                {
-                    if (param_count >= 3) continue;
-                    param_count++;
-
-                    int limit = std::atoi(msg.args[arg_idx].c_str());
-                    it->second->set_limit(limit);
-                    applied_modes += "l";
-                    applied_args += " " + msg.args[arg_idx];
-                    arg_idx++;
-                }
-                else if (!add)
-                {
-                    it->second->set_limit(-1);
-                    applied_modes += "l";
-                }
-            }
-            else if (c == 'o')
-            {
-                if (arg_idx < msg.args.size())
-                {
-                    if (param_count >= 3) continue;
-                    param_count++;
-
-                    std::string target_nick = msg.args[arg_idx];
-                    arg_idx++;
-
-                    Client* target_client = NULL;
-                    std::map<int, Client*>::iterator it_c;
-                    for (it_c = clients.begin(); it_c != clients.end(); ++it_c)
+                    else if (!add)
                     {
-                        if (it_c->second->get_nickname() == target_nick)
+                        if (arg_idx < msg.args.size())
                         {
-                            target_client = it_c->second;
-                            break;
+                            if (msg.args[arg_idx] == it->second->get_password())
+                            {
+                                it->second->set_password("");
+                                applied_args += " " + msg.args[arg_idx];
+                                mode_applied = true;
+                            }
+                            arg_idx++;
                         }
                     }
-
-                    if (!target_client)
-                        client->set_write_buffer(":localhost 401 " + client->get_nickname() + " " + target_nick + " :No such nick/channel\r\n");
-                    else if (!it->second->is_member(target_client))
-                        client->set_write_buffer(":localhost 441 " + client->get_nickname() + " " + target_nick + " " + target_channel + " :They aren't on that channel\r\n");
-                    else
+                }
+                else if (c == 'l')
+                {
+                    if (add && arg_idx < msg.args.size())
                     {
-                        if (add)
-                            it->second->add_operator(target_client);
-                        else
-                            it->second->remove_operator(target_client);
-                        
-                        applied_modes += "o";
-                        applied_args += " " + target_nick;
+                        if (param_count >= 3) continue;
+                        param_count++;
+
+                        int limit = std::atoi(msg.args[arg_idx].c_str());
+                        it->second->set_limit(limit);
+                        applied_args += " " + msg.args[arg_idx];
+                        mode_applied = true;
+                        arg_idx++;
+                    }
+                    else if (!add)
+                    {
+                        it->second->set_limit(-1);
+                        mode_applied = true;
                     }
                 }
+                else if (c == 'o')
+                {
+                    if (arg_idx < msg.args.size())
+                    {
+                        if (param_count >= 3) continue;
+                        param_count++;
+
+                        std::string target_nick = msg.args[arg_idx];
+                        arg_idx++;
+
+                        Client* target_client = NULL;
+                        std::map<int, Client*>::iterator it_c;
+                        for (it_c = clients.begin(); it_c != clients.end(); ++it_c)
+                        {
+                            if (it_c->second->get_nickname() == target_nick)
+                            {
+                                target_client = it_c->second;
+                                break;
+                            }
+                        }
+
+                        if (!target_client)
+                            client->set_write_buffer(":localhost 401 " + client->get_nickname() + " " + target_nick + " :No such nick/channel\r\n");
+                        else if (!it->second->is_member(target_client))
+                            client->set_write_buffer(":localhost 441 " + client->get_nickname() + " " + target_nick + " " + target_channel + " :They aren't on that channel\r\n");
+                        else
+                        {
+                            if (add)
+                                it->second->add_operator(target_client);
+                            else
+                                it->second->remove_operator(target_client);
+                            
+                            applied_args += " " + target_nick;
+                            mode_applied = true;
+                        }
+                    }
+                }
+                else
+                {
+                    client->set_write_buffer(":localhost 472 " + client->get_nickname() + " " + c + " :is unknown mode char to me for " + target_channel + "\r\n");
+                }
+
+                if (mode_applied)
+                {
+                    if (current_sign != last_appended_sign)
+                    {
+                        applied_modes += current_sign;
+                        last_appended_sign = current_sign;
+                    }
+                    applied_modes += c;
+                }
             }
-            else
-                client->set_write_buffer(":localhost 472 " + client->get_nickname() + " " + c + " :is unknown mode char to me for " + target_channel + "\r\n");
         }
 
-        if (!applied_modes.empty() && applied_modes != "+" && applied_modes != "-")
+
+        if (!applied_modes.empty())
         {
             std::string mode_msg = ":" + client->get_nickname() + "!" + client->get_username() + "@" + client->get_hostname() + " MODE " + target_channel + " " + applied_modes + applied_args + "\r\n";
             it->second->broadcast(mode_msg, NULL);
